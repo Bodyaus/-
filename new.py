185b192d3b36e724bccfce7a33452e6339ab4a08
Bodyaus <95971391+Bodyaus@users.noreply.github.com>
2024-09-12 17:59:33 +0300

    Add files via upload

---
+++ new.py
@@ -0,0 +1,11 @@
+from direct.showbase.ShowBase import ShowBase
+class Game(ShowBase):
+    def __init__(self):
+        ShowBase.__init__(self)
+        self.model = loader.loadModel('models/environment')
+        self.model.reparentTo(self.render)
+        self.model.setScale(0.1)
+        self.model.setPos(-2,25,-3)
+game = Game()
+
+game.run()
